77714ca0e788ec4a0452eb74302e944cca65212e
Leothurm <leonardo.thurm@gmail.com>
2023-01-23 18:30:06 -0300

    Continuação javaScript

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 let totalSlides = document.querySelectorAll(".slider-item").length;
+let currentSlide = 0;
 
 document.querySelector(".slider-container").style.width = `calc(100vw*${totalSlides})`;
 document.querySelector(".slider-buttons").style.height = ` ${document.querySelector('.slider').clientHeight}px`;
 
 
+function goPrev(){
+    currentSlide--
+    if(currentSlide <0){
+        currentSlide = totalSlides-1
+    }
+    updateMargin();
+}
+
+function goNext(){
+    currentSlide ++;
+    if(currentSlide > (totalSlides-1)){
+        currentSlide = 0;
+    }
+    updateMargin();
+}
+
+function updateMargin(){
+    let newMargin = (currentSlide * document.body.clientWidth);
+    document.querySelector('.slider-container').style.marginLeft = 
+    `-${newMargin}px`
+}
